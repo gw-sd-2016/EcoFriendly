783d7067d0a05bc4abd4f114a4eae5d3f011a5a8
ttunca <tomtunca@Toms-MacBook-Pro.local>
2015-11-11 19:53:15 -0500

    fix
    fix

---
+++ EcoFriendlySD/app/src/main/java/tunca/tom/ecofriendlyapp/SettingsActivity.java
@@ -0,0 +1,17 @@
+package tunca.tom.ecofriendlyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tomtunca on 11/7/15.
+ */
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInsteanceState){
+        super.onCreate(savedInsteanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+}
