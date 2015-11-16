11aa36cc341c7bb79ce683e76af977304acaa711
ttunca <tomtunca@Toms-MacBook-Pro.local>
2015-11-16 00:05:41 -0500

    updating settings UI
    I accidentally created another grade in my repo on my desktop, that is
    why you see the addition and deletion of those files.

--- EcoFriendlySD/Week2/app/src/main/java/tunca/tom/ecofriendlyapp/SettingsActivity.java
+++ EcoFriendlySD/Week2/app/src/main/java/tunca/tom/ecofriendlyapp/SettingsActivity.java
@@ -2,9 +2,10 @@ package tunca.tom.ecofriendlyapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends FragmentActivity {
 
     @Override
     protected void onCreate (Bundle savedInsteanceState){
