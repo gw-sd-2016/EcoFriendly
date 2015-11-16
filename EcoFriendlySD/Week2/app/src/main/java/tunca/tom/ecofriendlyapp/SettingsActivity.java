34abac2e57b811c57d1968f7113964db5eb5d2f4
ttunca <tomtunca@Toms-MacBook-Pro.local>
2015-11-15 23:11:32 -0500

    final app file
    learning how to make desktop same as website

---
+++ EcoFriendlySD/Week2/app/src/main/java/tunca/tom/ecofriendlyapp/SettingsActivity.java
@@ -0,0 +1,15 @@
+package tunca.tom.ecofriendlyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate (Bundle savedInsteanceState){
+        super.onCreate(savedInsteanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+}
